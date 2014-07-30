6ecc11beedd7a36736aa8d600e9e64a498220d33
Deepika Kamboj <deepikak@thoughtworks.com>
2014-07-30 14:07:47 +0530

    Adding Book model.

---
+++ main/src/model/Book.java
@@ -0,0 +1,6 @@
+package model;
+
+public class Book {
+  private Integer id;
+  private String name;
+}
---
+++ main/test/BookTest.java
@@ -0,0 +1,10 @@
+import model.Book;
+import org.junit.Test;
+
+public class BookTest {
+
+  @Test
+  public void createBook(){
+    Book book = new Book();
+  }
+}
